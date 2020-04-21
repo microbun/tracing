ef1fb37f169483b897e695c29b6ad322d188a547
microbun <microbun@live.com>
2020-04-21 23:10:17 +0800

    add error with

--- stackerr/error.go
+++ stackerr/error.go
@@ -1,37 +1,60 @@
 package stackerr
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"runtime"
 )
 
+type frame uintptr
+
+func (f frame) pc() uintptr {
+	return uintptr(f)
+}
+
+func (f frame) Format(s fmt.State, verb rune) {
+	pc := f.pc()
+	fn := runtime.FuncForPC(pc)
+	if fn != nil {
+		file, line := fn.FileLine(pc)
+		fmt.Fprintf(s, "%s\n\t%s:%d\n", fn.Name(), file, line)
+	} else {
+		fmt.Fprintf(s, "unkown stack\n")
+	}
+}
+
 //type stack uintptr
 type errorStack struct {
-	err   error
-	stack []uintptr
+	message string
+	cause   error
+	stack   []frame
 }
 
 func (e *errorStack) Error() string {
-	return e.err.Error()
+	if e.cause != nil && e.message == "" {
+		return e.cause.Error()
+	}
+	return e.message
 }
 
 func (e *errorStack) Format(s fmt.State, verb rune) {
 	switch verb {
 	case 'v':
 		// fmt.Fprintf(s, "%+v\n", e.err)
-		fmt.Fprintf(s, "error:%s\n", e.Error())
-		for _, pc := range e.stack {
-			fn := runtime.FuncForPC(pc)
+		fmt.Fprintf(s, "%s\n", e.Error())
+
+		for _, f := range e.stack {
+			fn := runtime.FuncForPC(f.pc())
 			if fn != nil {
-				file, line := fn.FileLine(pc)
-				fmt.Fprintf(s, "%s\n\t%s:%d\n", fn.Name(), file, line)
+
+				fmt.Fprintf(s, "%v", f)
 			} else {
 				fmt.Fprintf(s, "unkown stack\n")
 			}
 		}
-		// fallthrough
+		if v, ok := e.cause.(*errorStack); ok {
+			fmt.Fprintf(s, "cause by: %v", v)
+		}
 	case 's':
 		io.WriteString(s, e.Error())
 	case 'q':
@@ -43,8 +66,9 @@ func (e *errorStack) Format(s fmt.State, verb rune) {
 func New(message string) error {
 
 	return &errorStack{
-		err:   errors.New(message),
-		stack: callers(0),
+		message: message,
+		cause:   nil,
+		stack:   callers(0),
 	}
 }
 
@@ -53,8 +77,23 @@ func RawError(err error) error {
 	if err == nil {
 		return err
 	}
+	for {
+		if e, ok := err.(*errorStack); ok {
+			err = e.cause
+		} else {
+			return err
+		}
+	}
+
+}
+
+//Cause return a cuase
+func Cause(err error) error {
+	if err == nil {
+		return err
+	}
 	if e, ok := err.(*errorStack); ok {
-		return e.err
+		return e.cause
 	}
 	return err
 }
@@ -66,43 +105,30 @@ func WithStack(err error) error {
 	}
 	if e, ok := err.(*errorStack); ok {
 		pc, _, _, _ := runtime.Caller(2)
-		e.stack = append(e.stack, pc)
+		e.stack = append(e.stack, frame(pc))
 		return e
 	}
-	// caller := [8]uintptr{}
 
 	return &errorStack{
-		err:   err,
+		cause: err,
 		stack: callers(0),
 	}
 }
 
-func callers(n int) []uintptr {
+func WithMessage(err error, message string) error {
+	return &errorStack{
+		message: message,
+		cause:   err,
+		stack:   callers(0),
+	}
+}
+
+func callers(n int) []frame {
 	n = n + 3
 	pc2, _, _, _ := runtime.Caller(n)
 	pc1, _, _, _ := runtime.Caller(n - 1)
-	pcs := make([]uintptr, 0)
-	pcs = append(pcs, pc1)
-	pcs = append(pcs, pc2)
+	pcs := make([]frame, 0)
+	pcs = append(pcs, frame(pc1))
+	pcs = append(pcs, frame(pc2))
 	return pcs
 }
-
-// //PrintStack print a stack trace
-// func PrintStack(err error) {
-// 	fmt.Printf("error: %s\n", err.Error())
-// 	if e, ok := err.(*errorStack); ok {
-// 		for _, pc := range e.stack {
-// 			printPC(pc)
-// 		}
-// 	}
-// }
-
-// func printPC(pc uintptr) {
-// 	fn := runtime.FuncForPC(pc)
-// 	if fn != nil {
-// 		file, line := fn.FileLine(pc)
-// 		fmt.Printf("%s\n\t%s:%d\n", fn.Name(), file, line)
-// 	} else {
-// 		fmt.Printf("unkown stack\n")
-// 	}
-// }
